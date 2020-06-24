d56daa8e47fb6f2e79a5635bb45acf0bac199320
Abel Wang <abel.wang@microsoft.com>
2020-06-23 18:47:05 -0700

    made my first change to print out myInput

--- src/main.ts
+++ src/main.ts
@@ -3,14 +3,8 @@ import {wait} from './wait'
 
 async function run(): Promise<void> {
   try {
-    const ms: string = core.getInput('milliseconds')
-    core.debug(`Waiting ${ms} milliseconds ...`)
-
-    core.debug(new Date().toTimeString())
-    await wait(parseInt(ms, 10))
-    core.debug(new Date().toTimeString())
-
-    core.setOutput('time', new Date().toTimeString())
+    const myInput: string = core.getInput('myInput')
+    core.debug(`Hello ${myInput}  ...`)
   } catch (error) {
     core.setFailed(error.message)
   }
